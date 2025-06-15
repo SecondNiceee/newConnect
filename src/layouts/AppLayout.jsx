29d52c680f948631c5ede70462d00d509bce9ecc
SecondNiceee <col1596321@gmail.com>
2025-06-16 01:53:31 +0300

    asd

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -4,12 +4,14 @@ import icon from "../images/icons/connectHeader.svg";
 
 const AppLayout = () => {
     return (
-    <div className="flex  flex-col">
-      <div className="pt-[60px] bg-black pb-[15px] gap-[6px] justify-center flex items-center">
+    <div className="flex flex-col">
+      <div className="fixed z-[999999999] top-0 h-[80px] w-full bg-black gap-[6px] justify-center flex items-center">
         <img className="w-[24px] h-[24px]" src={icon} alt="headerIcon"/>
         <h1 className="text-white text-[20px] font-sf-pro-display-600 leading-[24.726px] tracking-[0.411px]">Connect</h1>
       </div>
-      <Outlet /> {/* Здесь будут UserProfile или UserPosts */}
+      <div className="pt-[60px]">
+        <Outlet  /> {/* Здесь будут UserProfile или UserPosts */}
+      </div>
     </div>
     );
 };
