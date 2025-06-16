8e12a9f3ab5dcbe418156a18420d6f23fff0479e
SecondNiceee <col1596321@gmail.com>
2025-06-17 01:54:57 +0300

    asd

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -4,19 +4,19 @@ import { isIphone } from "../functions/isIphone";
 
 
 const AppLayout = () => {
-    // if (!isIphone()){
-    //     return <Outlet />    
-    // }
-    // return (
-    // <div className="flex flex-col">
-    //   <div className="fixed left-0 z-[999999999] pb-[9.5px] top-0 h-[95px] w-full bg-[#18222D] gap-[6px] justify-center flex items-end">
-    //     <img  src={icon} alt="headerIcon"/>
-    //   </div>
-    //   <div className="pt-[95px] h-full overflow-auto">
-    //     <Outlet  /> {/* Здесь будут UserProfile или UserPosts */}
-    //   </div>
-    // </div>
-    // );
+    if (!isIphone()){
+        return <Outlet />    
+    }
+    return (
+    <div className="flex flex-col">
+      <div className="fixed left-0 z-[999999999] pb-[9.5px] top-0 h-[95px] w-full bg-[#18222D] gap-[6px] justify-center flex items-end">
+        <img  src={icon} alt="headerIcon"/>
+      </div>
+      <div className="pt-[95px] h-full overflow-auto">
+        <Outlet  /> {/* Здесь будут UserProfile или UserPosts */}
+      </div>
+    </div>
+    );
     return <Outlet />
 };
 
