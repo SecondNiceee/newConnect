c16025c0b6bb32608dbadc0931497e407adac187
SecondNiceee <col1596321@gmail.com>
2025-06-16 04:35:53 +0300

    asd

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -1,13 +1,12 @@
 import { Outlet } from "react-router";
-import icon from "../images/icons/connectHeader.svg";
+import icon from "../images/icons/Logo_Header.svg";
 
 
 const AppLayout = () => {
     return (
     <div className="flex flex-col">
       <div className="fixed z-[999999999] pb-[0px] top-0 h-[84px] w-full bg-[#18222D] gap-[6px] justify-center flex items-end">
-        <img className="w-[24px] h-[24px]" src={icon} alt="headerIcon"/>
-        <h1 className="text-white text-[20px] font-sf-pro-display-600 leading-[24.726px] tracking-[0.411px]">Connect</h1>
+        <img  src={icon} alt="headerIcon"/>
       </div>
       <div className="pt-[84px]">
         <Outlet  /> {/* Здесь будут UserProfile или UserPosts */}
