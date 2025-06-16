7a6cea99ddfa4eff7e7ad12fd164394ae63577cd
SecondNiceee <col1596321@gmail.com>
2025-06-16 04:42:20 +0300

    asdas

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -5,7 +5,7 @@ import icon from "../images/icons/Logo_Header.svg";
 const AppLayout = () => {
     return (
     <div className="flex flex-col">
-      <div className="fixed z-[999999999] pb-[0px] top-0 h-[84px] w-full bg-[#18222D] gap-[6px] justify-center flex items-end">
+      <div className="fixed z-[999999999] pb-[0px] top-0 h-[95px] w-full bg-[#18222D] gap-[6px] justify-center flex items-end">
         <img  src={icon} alt="headerIcon"/>
       </div>
       <div className="pt-[84px]">
