c16fab073e167e63098a2328c6d777f92808aa56
SecondNiceee <col1596321@gmail.com>
2025-06-17 03:04:44 +0300

    asd

--- src/layouts/AppLayout.jsx
+++ src/layouts/AppLayout.jsx
@@ -12,7 +12,7 @@ const AppLayout = () => {
       <div className="fixed left-0 z-[999999999] pb-[9.5px] top-0 h-[95px] w-full bg-[#18222D] gap-[6px] justify-center flex items-end">
         <img  src={icon} alt="headerIcon"/>
       </div>
-      <div className="pt-[95px] flex flex-col h-full overflow-auto">
+      <div className="pt-[95px] flex flex-col h-full">
         <Outlet  /> {/* Здесь будут UserProfile или UserPosts */}
       </div>
     </div>
